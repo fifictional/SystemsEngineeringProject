89c52f59c0c57c4d15e8b46576d08731db390518
Thun78 <158178989+Thun78@users.noreply.github.com>
2026-01-23 13:44:30 +0000

    Implement kalman_demo_PID for PID control simulation
    Added kalman_demo_PID function for PID control with Kalman filter and sensor noise.

--- demos.py
+++ demos.py
@@ -74,3 +74,45 @@ def run_linear_vs_nonlinear_demo(params, z0, u=0.0, dt=0.001, tf=5.0, plot=True)
         plt.show()
 
     return t, z_nl, z_lin
+
+def kalman_demo_PID(params, z0):
+    dt = 0.02
+    tf = 10
+    t = np.arange(0, tf, dt)
+    z = np.array(z0, dtype=float)
+
+    sensor = Sensor(pos_std=0.01, angle_std=0.05)
+    kf = KalmanFilter(dt)
+    kf.x = np.array([0.05, 0, np.deg2rad(10), 0])
+
+    # Just for tuning
+    theta_error = np.zeros(len(t))
+    true_states = np.zeros((len(t), 4))
+    noisy_meas = np.zeros((len(t), 2))
+    filtered_states = np.zeros((len(t), 4))
+    controls = np.zeros(len(t))
+
+    theta_ref = 0
+    pid = PID_controller(Kp=70, Ki=0, Kd=22, N=10)
+
+    for i, ti in enumerate(t):
+        true_states[i] = z
+
+        y = sensor.add_noise(z[[0, 2]])
+        noisy_meas[i] = y
+        kf.update(y)
+
+        theta_hat = kf.x[2]
+        error = theta_ref - theta_hat
+        theta_error[i] = error
+        u = -pid.step(theta_ref, theta_hat, dt)
+        controls[i] = u
+
+        kf.predict(u=u)
+        filtered_states[i] = kf.x
+
+        z = rk4_step(nonlinear_dynamics, ti, z, dt, u, params)
+
+    plot_pid_performance(t, theta_error)
+    animate_with_kalman(t, true_states, filtered_states)
+    return t, true_states, noisy_meas, filtered_states, controls, theta_error
