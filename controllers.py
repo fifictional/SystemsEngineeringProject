b5095bf438fa4f8910a1cbebd34045f44bbd16bc
Thun78 <158178989+Thun78@users.noreply.github.com>
2026-01-23 13:43:52 +0000

    Add PID controller class to controllers.py

--- controllers.py
+++ controllers.py
@@ -45,3 +45,28 @@ class LQRController:
             u = np.clip(u, -self.u_max, self.u_max)
 
         return u
+
+class PID_controller:
+    def __init__(self, Kp, Ki, Kd, N=10):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.integral = 0
+        self.prev_error = 0
+        self.d_filt = 0
+        self.N = N
+
+    def step(self, setpoint, measurement, dt):    
+        error = setpoint - measurement
+
+        self.integral += error * dt
+
+        Tf = 1 / self.N
+        alpha = Tf / (Tf + dt)
+        de_dt = (error - self.prev_error) / dt
+        self.d_filt = alpha * self.d_filt + (1 - alpha) * de_dt
+
+        output = self.Kp * error + self.Ki * self.integral + self.Kd * self.d_filt
+        self.prev_error = error
+
+        return output
