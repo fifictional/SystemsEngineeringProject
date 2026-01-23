e57bb54833a0a4d4a7bc124ee727293108c328b0
Thun78 <158178989+Thun78@users.noreply.github.com>
2026-01-23 13:45:55 +0000

    Call kalman_demo_PID with initial parameters

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ if __name__ == "__main__":
     # run_kalman_demo(params)
 
     z0 = [0, 0, np.deg2rad(5), 0]
+    
+    kalman_demo_PID(params, z0)
+    
     t, z_nl, z_lin = run_linear_vs_nonlinear_demo(params, z0)
 
     z0_large = [0, 0, np.deg2rad(20), 0]
